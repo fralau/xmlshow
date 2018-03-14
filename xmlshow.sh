2a7e443398d7937811882f67974c266e17cfc5bb
Laurent Franceschetti <36767124+fralau@users.noreply.github.com>
2018-03-14 18:17:40 +0100

    Update xmlshow.sh

--- xmlshow.sh
+++ xmlshow.sh
@@ -4,6 +4,8 @@
 # Copyright (c) 2018 Laurent Franceschetti, MIT License
 #
 
+# in case you need to change the format, see -out-format in highlight:
+OUT_FORMAT=xterm256
 
 filename=$1
 remove=false
@@ -18,10 +20,16 @@ then
     filename=$(mktemp).xml
     unzip -p "$1" > "$filename"
     remove=true
+elif [[ $mimetype = 'application/x-gzip' ]]
+then
+    echo "Gunzipping..."
+    filename=$(mktemp).xml
+    gunzip -c "$1" > "$filename"
+    remove=true
 fi
 
 # Beautify, highlight with colors and send to less
-xmllint --format "$filename" | highlight --syntax=xml --out-format=truecolor | less -R -N
+xmllint --format "$filename" | highlight --syntax=xml --out-format=$OUT_FORMAT | less -R -N
 
 # remove the filename
 if $remove
